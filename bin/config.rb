a6f5ae54122f0da0eadac3dc22461085c224effe
Jos Spencer <crazyandroid101@gmail.com>
2016-12-15 11:41:53 +0800

    Added IP to config for binding

--- bin/config.rb
+++ bin/config.rb
@@ -2,3 +2,4 @@ $domain = 'localhost:8080'# or base URL
 $savefolder = 'p'# Where the files are to be saved
 $banned_ext = %w(.exe .msi .scr .cpl) # Banned extensions/files
 $code = '' # Code that all users must use to upload their files
+$ip = ''# Server IP
