41e528fa23111b899a898896cd1fc0032a9f6c9d
Jos Spencer <crazyandroid101@gmail.com>
2016-12-15 11:48:08 +0800

    Set to port 80 to match previous commit

--- bin/config.rb
+++ bin/config.rb
@@ -1,4 +1,4 @@
-$domain = 'localhost:8080'# or base URL
+$domain = 'localhost:80'# or base URL
 $savefolder = 'p'# Where the files are to be saved
 $banned_ext = %w(.exe .msi .scr .cpl) # Banned extensions/files
 $code = '' # Code that all users must use to upload their files
