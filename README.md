cca7af8ce30371c012aea26b17326e3e7eeb25ab
Jos Spencer <crazyandroid101@gmail.com>
2016-12-15 10:40:01 +0800

    First basic setup

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-nekogirls-rb
+#Nekogirls-rb
+
+
+##ShareX Example
+```
+{
+  "Name": "nekogirls.moe",
+  "RequestType": "POST",
+  "RequestURL": "http://nekogirls.moe/upload",
+  "FileFormName": "file_to_upload",
+  "Arguments": {
+    "reqtype": "code"
+  },
+  "ResponseType": "Text"
+}
---
+++ bin/nekogirls.rb
@@ -0,0 +1,61 @@
+require 'bundler/setup'
+require 'date'
+require 'sinatra'
+require 'haml'
+
+set :port, 8080
+set :static, true
+# set :public_folder, 'static'
+set :views, 'views'
+
+require_relative 'config.rb'
+
+get '/' do
+  '<h2> Personal image hosting website <a href="https://github.com/Cyan101/nekogirls-rb">Nekogirls-rb</a></h2>'
+  # Temporary "Home" Page
+end
+
+get '/p/:file' do
+  send_file('./public/p/' + params[:file])
+end
+
+get '/upload/?' do
+  haml :upload, format: :html5
+end
+
+post '/upload' do
+  fileid = get_unqiue_id
+  filetype = File.extname(params['file_to_upload'][:filename])
+  unless $banned_ext.include?(filetype.downcase)
+    File.open('./public/p/' + fileid + filetype, 'w') do |f|
+      f.write(params['file_to_upload'][:tempfile].read)
+    end
+  end
+  return "#{$domain}/#{$savefolder}/#{fileid}#{filetype}"
+end
+
+not_found do
+  status 404
+  '<h2>shit erm... we seem to have lost that page....</h2>' # Temporary 404 Page
+end
+
+
+
+#---------------------------------------------------
+def get_unqiue_id
+  return DateTime.now.strftime('%Q').to_i.to_s52
+end
+
+class Numeric
+  Alpha52 = ("a".."z").to_a + ("A".."Z").to_a
+  def to_s52
+    return "" if self < 1
+    s, q = "", self
+    loop do
+      q, r = (q - 1).divmod(52)
+      s.prepend(Alpha52[r])
+      break if q.zero?
+    end
+    s
+  end
+end
