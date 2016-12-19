92afc02487522912d31d581f0dbc5bdf305681b9
Jos Spencer <crazyandroid101@gmail.com>
2016-12-20 02:10:11 +0800

    Cleanup

--- bin/config.rb
+++ bin/config.rb
@@ -1,5 +1,5 @@
 $domain = 'localhost:80'# or base URL
-$savefolder = 'p'# Where the files are to be saved
+$savefolder = 'p'# Folder where the files are to be saved
 $banned_ext = %w(.exe .msi .scr .cpl) # Banned extensions/files
 $code = '' # Code that all users must use to upload their files
 $ip = ''# Server IP
