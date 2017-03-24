3cf1aae052a856079b3b3b659ec648da8304fb6d
Jos Spencer <crazyandroid101@gmail.com>
2017-03-24 20:51:25 +0800

    http or https option no longer needed

--- bin/config.rb
+++ bin/config.rb
@@ -1,5 +1,4 @@
 $domain = 'localhost:80'# or base URL
-$httptype = 'http'# or 'https'
 $savefolder = 'p'# Folder where the files are to be saved
 $banned_ext = %w(.exe .msi .scr .cpl) # Banned extensions/files
 $servercode = '' # Code that all users must use to upload their files
